162751837c0054b31588c194992811704bea5e0c
Stefan Saraev <stefan@saraev.ca>
2014-08-18 17:46:54 +0300

    setup timezone
    on TZ country setting change, store the value in format
    TIMEZONE=Xx/Yyy to /storage/.cache/timezone to be used with
    tz-data.service
    kodi-100.08-setup-timezone.patch

--- xbmc/platform/posix/PosixTimezone.cpp
+++ xbmc/platform/posix/PosixTimezone.cpp
@@ -25,6 +25,8 @@
 
 #include <algorithm>
 
+#include <fstream>
+
 CPosixTimezone::CPosixTimezone()
 {
    char* line = NULL;
@@ -142,6 +144,16 @@ void CPosixTimezone::OnSettingChanged(const std::shared_ptr<const CSetting>& set
   const std::string &settingId = setting->GetId();
   if (settingId == CSettings::SETTING_LOCALE_TIMEZONE)
   {
+    std::ofstream out("/storage/.cache/timezone");
+    if (out)
+    {
+      const std::string tz = std::string("TIMEZONE=") + (std::static_pointer_cast<const CSettingString>(setting)->GetValue().c_str());
+      out << tz << std::endl;
+      out.flush();
+      out.close();
+      system("systemctl restart tz-data.service");
+    }
+
     SetTimezone(std::static_pointer_cast<const CSettingString>(setting)->GetValue());
 
     CDateTime::ResetTimezoneBias();
