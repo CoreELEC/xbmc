2c629caf94717ebf0d9bca89d3fcab344c06d1d6
anaconda <anaconda@menakite.eu>
2015-02-25 18:22:21 +0100

    Load OSD dialogs on startup.
    Fixes skipped frames the first time they're loaded in memory on less powered
    devices, like a Raspberry Pi, when using DVDPlayer.
    See http://forum.kodi.tv/showthread.php?tid=211501&pid=1938811#pid1938811

--- xbmc/video/dialogs/GUIDialogAudioSettings.cpp
+++ xbmc/video/dialogs/GUIDialogAudioSettings.cpp
@@ -45,7 +45,9 @@
 
 CGUIDialogAudioSettings::CGUIDialogAudioSettings()
   : CGUIDialogSettingsManualBase(WINDOW_DIALOG_AUDIO_OSD_SETTINGS, "DialogSettings.xml")
-{ }
+{
+  m_loadType = LOAD_ON_GUI_INIT;
+}
 
 CGUIDialogAudioSettings::~CGUIDialogAudioSettings() = default;
 
