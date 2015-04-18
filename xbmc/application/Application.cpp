30e73d4e9c0f591a4856ca62887fb8dad9ef226f
Stefan Saraev <stefan@saraev.ca>
2015-04-18 14:59:29 +0300

    disable minimize kodi-100.07-disable-minimize.patch

--- xbmc/application/Application.cpp
+++ xbmc/application/Application.cpp
@@ -1591,7 +1591,6 @@ void CApplication::OnApplicationMessage(ThreadMessage* pMsg)
     break;
 
   case TMSG_MINIMIZE:
-    CServiceBroker::GetWinSystem()->Minimize();
     break;
 
   case TMSG_EXECUTE_OS:
