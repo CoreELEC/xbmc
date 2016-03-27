494313b4a63ce340a1b15ef8418bd604c78f6e03
Chris "Koying" Browet <cbro@semperpax.com>
2016-03-27 16:36:42 +0200

    FIX: speedup thumb loading

--- xbmc/video/VideoThumbLoader.cpp
+++ xbmc/video/VideoThumbLoader.cpp
@@ -19,6 +19,8 @@
 #include "filesystem/DirectoryCache.h"
 #include "filesystem/StackDirectory.h"
 #include "guilib/GUIComponent.h"
+#include "filesystem/File.h"
+#include "filesystem/CurlFile.h"
 #include "guilib/GUIWindowManager.h"
 #include "guilib/StereoscopicsManager.h"
 #include "music/MusicDatabase.h"
@@ -440,11 +442,28 @@ bool CVideoThumbLoader::LoadItemLookup(CFileItem* pItem)
   // We can only extract flags/thumbs for file-like items
   if (!pItem->m_bIsFolder && pItem->IsVideo())
   {
-    // An auto-generated thumb may have been cached on a different device - check we have it here
     std::string url = pItem->GetArt("thumb");
-    if (StringUtils::StartsWith(url, "image://video@") &&
-        !CServiceBroker::GetTextureCache()->HasCachedImage(url))
-      pItem->SetArt("thumb", "");
+    if (!CServiceBroker::GetTextureCache()->HasCachedImage(url))
+    {
+      // An auto-generated thumb may have been cached on a different device - check we have it here
+      if (StringUtils::StartsWith(url, "image://video@"))
+        pItem->SetArt("thumb", "");
+
+      // Check invalid redirections
+      else if (URIUtils::IsInternetStream(url))
+      {
+        std::string mimetype;
+        bool valid = XFILE::CCurlFile::GetMimeType(CURL(url), mimetype);
+        if (valid && !StringUtils::StartsWith(mimetype, "image/"))
+          valid = false;
+        if (!valid)
+          pItem->SetArt("thumb", "");
+      }
+
+      // Check if thumb source still exists
+      else if (!XFILE::CFile::Exists(url))
+        pItem->SetArt("thumb", "");
+    }
 
     const std::shared_ptr<CSettings> settings = CServiceBroker::GetSettingsComponent()->GetSettings();
     if (!pItem->HasArt("thumb"))
