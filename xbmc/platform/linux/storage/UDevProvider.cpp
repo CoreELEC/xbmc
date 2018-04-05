f6c69c87810f5a81f5d0439a09b0ff5e30b75b5d
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-04-05 11:42:13 +0100

    use udevil to mount kodi-100.04-use-udevil-to-umount.patch

--- xbmc/platform/linux/storage/UDevProvider.cpp
+++ xbmc/platform/linux/storage/UDevProvider.cpp
@@ -194,7 +194,7 @@ bool CUDevProvider::Eject(const std::string& mountpath)
 {
   // just go ahead and try to umount the disk
   // if it does umount, life is good, if not, no loss.
-  std::string cmd = "umount \"" + mountpath + "\"";
+  std::string cmd = "udevil umount \"" + mountpath + "\"";
   int status = system(cmd.c_str());
 
   if (status == 0)
