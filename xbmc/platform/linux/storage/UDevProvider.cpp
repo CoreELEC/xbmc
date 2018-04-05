69584d758f12820fed372a37c9a11bd65f6bcffe
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-04-05 11:42:48 +0100

    udevprovider: filter out OE specific mounts kodi-100.13-udevprovider-filter-out-OE-specific-mounts.patch

--- xbmc/platform/linux/storage/UDevProvider.cpp
+++ xbmc/platform/linux/storage/UDevProvider.cpp
@@ -150,6 +150,20 @@ void CUDevProvider::GetDisks(std::vector<CMediaSource>& disks, bool removable)
       continue;
     }
 
+    // LE: filter out Libreelec specific mounts
+    if (strstr(mountpoint, "/flash"))
+    {
+      udev_device_unref(device);
+      continue;
+    }
+
+    if (strstr(mountpoint, "/storage"))
+    {
+      udev_device_unref(device);
+      continue;
+    }
+    // OE
+
     // look for devices on the usb bus, or mounted on */media/ (sdcards), or optical devices
     const char *bus = udev_device_get_property_value(device, "ID_BUS");
     const char *optical = udev_device_get_property_value(device, "ID_CDROM"); // matches also DVD, Blu-ray
