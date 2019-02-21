2c234a6aece832a18b8f91ebb2f24fd734254947
adamg <adamg88@users.noreply.github.com>
2019-02-21 00:41:31 +0000

    CoreELEC Settings

--- xbmc/windowing/amlogic/WinSystemAmlogic.cpp
+++ xbmc/windowing/amlogic/WinSystemAmlogic.cpp
@@ -82,6 +82,52 @@ CWinSystemAmlogic::~CWinSystemAmlogic()
 
 bool CWinSystemAmlogic::InitWindowSystem()
 {
+  if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_COREELEC_AMLOGIC_NOISEREDUCTION))
+  {
+     CLog::Log(LOGDEBUG, "CWinSystemAmlogic::InitWindowSystem -- disabling noise reduction");
+     SysfsUtils::SetString("/sys/module/di/parameters/nr2_en", "0");
+  }
+
+  std::string attr = "";
+  SysfsUtils::GetString("/sys/class/amhdmitx/amhdmitx0/attr", attr);
+  //We delay writing attr until everything is done with it to avoid multiple display resets.
+  if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_COREELEC_AMLOGIC_FORCE422))
+  {
+     CLog::Log(LOGDEBUG, "CWinSystemAmlogic::InitWindowSystem -- Setting 422 output");
+     if (attr.find("444") != std::string::npos ||
+         attr.find("422") != std::string::npos ||
+         attr.find("420") != std::string::npos)
+       attr.replace(attr.find("4"),3,"422");
+     else
+       attr.append("422");
+  }
+  if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_COREELEC_AMLOGIC_LIMIT8BIT))
+  {
+     CLog::Log(LOGDEBUG, "CWinSystemAmlogic::InitWindowSystem -- Limiting display to 8bit colour depth");
+     if (attr.find("10bit") != std::string::npos)
+       attr.replace(attr.find("10bit"),5,"8bit");
+     else
+       attr.append("8bit");
+  }
+  if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_COREELEC_AMLOGIC_LIMIT8BIT) ||
+      CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_COREELEC_AMLOGIC_FORCE422))
+  {
+     //attr.append("now");
+     SysfsUtils::SetString("/sys/class/amhdmitx/amhdmitx0/attr", attr.c_str());
+  }
+
+  if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_COREELEC_AMLOGIC_MUTEHDMIAUDIO))
+  {
+     CLog::Log(LOGDEBUG, "CWinSystemAmlogic::InitWindowSystem -- muting hdmi audio");
+     SysfsUtils::SetString("/sys/class/amhdmitx/amhdmitx0/config", "audio_off");
+  }
+
+  if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_COREELEC_AMLOGIC_MAXGPUCLOCK))
+  {
+     CLog::Log(LOGDEBUG, "CWinSystemAmlogic::InitWindowSystem -- increasing GPU clock rate");
+     SysfsUtils::SetString("/sys/class/mpgpu/scale_mode", "2");
+  }
+
   m_nativeDisplay = EGL_DEFAULT_DISPLAY;
 
   CDVDVideoCodecAmlogic::Register();
