723e94bfcb7964064a6c0dd12cd6feb161982314
afl1 <afl2001@gmail.com>
2019-03-11 19:38:47 +0100

    VideoPlayer: SD channels assume 16:9
    Assume 16:9 aspect ratio for SD at playback start.
    Real aspec ratio will be fix later from stream.
    
    Patch: kodi-le-037-SD-channels-assume-16-9.patch

--- xbmc/utils/BitstreamConverter.cpp
+++ xbmc/utils/BitstreamConverter.cpp
@@ -1788,7 +1788,7 @@ bool CBitstreamConverter::h264_sequence_header(const uint8_t *data, const uint32
                     break;
                 case 4:
                     // 16:11
-                    ratio *= 1.4545454545454546;
+                    ratio *= 1.4222222222222222;
                     break;
                 case 5:
                     // 40:33
@@ -1851,6 +1851,11 @@ bool CBitstreamConverter::h264_sequence_header(const uint8_t *data, const uint32
                 changed = true;
                 sequence->ratio = ratio;
                 sequence->ratio_info = aspect_ratio_idc;
+            }
+            if (changed)
+            {
+              CLog::Log(LOGDEBUG, "CBitstreamConverter::h264_sequence_header: "
+                "width({:d}), height({:d}), ratio({:f}), {:d}x{:d}", pic_width_cropped, pic_height_cropped, ratio, sar_width, sar_height);
             }
 
             break;
