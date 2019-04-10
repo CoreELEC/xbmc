d7ef8a72cf4b25508f36d307b6f336d7b7c8194d
Portisch <hugo.portisch@yahoo.de>
2019-04-10 07:14:37 +0000

    Render: keep FIFO order on late frames

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
@@ -1115,13 +1115,15 @@ void CRenderManager::PrepareNextRender()
     // skip late frames
     while (m_queued.front() != idx)
     {
+      m_presentsourcePast = m_queued.front();
+      m_queued.pop_front();
+
       if (m_presentsourcePast >= 0)
       {
         m_discard.push_back(m_presentsourcePast);
         m_QueueSkip++;
+        m_presentsourcePast = -1;
       }
-      m_presentsourcePast = m_queued.front();
-      m_queued.pop_front();
     }
 
     const int lateframes = static_cast<int>((renderPts - m_Queue[idx].pts) *
