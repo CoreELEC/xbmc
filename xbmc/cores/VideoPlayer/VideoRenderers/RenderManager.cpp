241174277a70ba09b107f88f9cbda5e5fefbaad4
Portisch <hugo.portisch@yahoo.de>
2019-04-10 10:23:17 +0000

    Render: rework late frame counter Now the late frame counter depends on the skipped frames count

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
@@ -1193,7 +1193,7 @@ void CRenderManager::PrepareNextRender()
     // see if any future queued frames are already due
     auto iter = m_queued.begin();
     int idx = *iter;
-    ++iter;
+    int lateframes = 0;
     while (iter != m_queued.end())
     {
       // the slot for rendering in time is [pts .. (pts +  x * frametime)]
@@ -1203,6 +1203,7 @@ void CRenderManager::PrepareNextRender()
       double x = (m_lateframes <= 6) ? 0.98 : 0;
       if (renderPts < m_Queue[*iter].pts + x * frametime)
         break;
+      lateframes++;
       idx = *iter;
       ++iter;
     }
@@ -1221,8 +1222,6 @@ void CRenderManager::PrepareNextRender()
       }
     }
 
-    int lateframes = static_cast<int>((renderPts - m_Queue[idx].pts) *
-                                      static_cast<double>(m_fps / DVD_TIME_BASE));
     if (lateframes)
       m_lateframes += lateframes;
     else
