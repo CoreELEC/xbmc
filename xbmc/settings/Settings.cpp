fb99e00b5aeb1d56532e65e20fff5c72ea8a0de8
Arthur Liberman <arthur_liberman@hotmail.com>
2019-04-12 04:41:02 +0300

    aml-settings: add HDR to SDR tone mapping configuration

--- xbmc/settings/Settings.cpp
+++ xbmc/settings/Settings.cpp
@@ -416,6 +416,7 @@ constexpr const char* CSettings::SETTING_MASTERLOCK_STARTUPLOCK;
 constexpr const char* CSettings::SETTING_MASTERLOCK_MAXRETRIES;
 constexpr const char* CSettings::SETTING_COREELEC_AMLOGIC_NOISEREDUCTION;
 constexpr const char* CSettings::SETTING_COREELEC_AMLOGIC_SDR2HDR;
+constexpr const char* CSettings::SETTING_COREELEC_AMLOGIC_HDR2SDR;
 constexpr const char* CSettings::SETTING_CACHE_HARDDISK;
 constexpr const char* CSettings::SETTING_CACHEVIDEO_DVDROM;
 constexpr const char* CSettings::SETTING_CACHEVIDEO_LAN;
