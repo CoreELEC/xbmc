f20c6c9104e2d35aeb54b05075a3e1916b87ab98
adamg <adamg88@users.noreply.github.com>
2019-04-14 14:52:16 +0100

    NetworkServices: silence race condition error, sometimes if Kodi is started before the network interface is up then Kodi can not bind to the necessary ports and produces the communication server error

--- xbmc/network/NetworkServices.cpp
+++ xbmc/network/NetworkServices.cpp
@@ -523,10 +523,10 @@ void CNetworkServices::Start()
   StartZeroconf();
   if (m_settings->GetBool(CSettings::SETTING_SERVICES_UPNP))
     StartUPnP();
-  if (m_settings->GetBool(CSettings::SETTING_SERVICES_ESENABLED) && !StartEventServer())
-    CGUIDialogKaiToast::QueueNotification(CGUIDialogKaiToast::Warning, g_localizeStrings.Get(33102), g_localizeStrings.Get(33100));
-  if (m_settings->GetBool(CSettings::SETTING_SERVICES_ESENABLED) && !StartJSONRPCServer())
-    CGUIDialogKaiToast::QueueNotification(CGUIDialogKaiToast::Warning, g_localizeStrings.Get(33103), g_localizeStrings.Get(33100));
+  if (m_settings->GetBool(CSettings::SETTING_SERVICES_ESENABLED)) {
+    StartEventServer();
+    StartJSONRPCServer();
+  }
 
 #ifdef HAS_WEB_SERVER
   // Start web server after eventserver and JSON-RPC server, so users can use these interfaces
