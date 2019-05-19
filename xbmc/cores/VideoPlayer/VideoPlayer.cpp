4a89963e7362b777aee83531c7f54ba07d1f316d
afl1 <afl2001@gmail.com>
2019-05-19 19:01:45 +0200

    VideoPlayer: remove unnecessary delay at playback start

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -2216,26 +2216,26 @@ void CVideoPlayer::HandlePlaySpeed()
       else if (m_CurrentAudio.starttime != DVD_NOPTS_VALUE && m_CurrentAudio.packets > 0)
       {
         if (m_pInputStream->IsRealtime())
-          clock = m_CurrentAudio.starttime - m_CurrentAudio.cachetime - DVD_MSEC_TO_TIME(1000);
+          clock = m_CurrentAudio.starttime - m_CurrentAudio.cachetime - DVD_MSEC_TO_TIME(800);
         else
           clock = m_CurrentAudio.starttime - m_CurrentAudio.cachetime;
 
         if (m_CurrentVideo.starttime != DVD_NOPTS_VALUE && (m_CurrentVideo.packets > 0))
         {
-          if (m_CurrentVideo.starttime - m_CurrentVideo.cachetotal - DVD_MSEC_TO_TIME(1200) < clock)
+          if (m_CurrentVideo.starttime - m_CurrentVideo.cachetotal < clock)
           {
-            clock = m_CurrentVideo.starttime - m_CurrentVideo.cachetotal - DVD_MSEC_TO_TIME(1200);
+            clock = m_CurrentVideo.starttime - m_CurrentVideo.cachetotal;
           }
-          else if (m_CurrentVideo.starttime - DVD_MSEC_TO_TIME(1800) > m_CurrentAudio.starttime &&
+          else if (m_CurrentVideo.starttime > m_CurrentAudio.starttime &&
                    !m_pInputStream->IsRealtime())
           {
             const int audioLevel = m_VideoPlayerAudio->GetLevel();
             const double audioTimeMs = m_messageQueueTimeSize * 1000.0 * audioLevel / 100.0;
             const double maxAudioTime = clock + DVD_MSEC_TO_TIME(audioTimeMs);
-            if ((m_CurrentVideo.starttime - m_CurrentVideo.cachetotal - DVD_MSEC_TO_TIME(1800)) > maxAudioTime)
+            if ((m_CurrentVideo.starttime - m_CurrentVideo.cachetotal) > maxAudioTime)
               clock = maxAudioTime;
             else
-              clock = m_CurrentVideo.starttime - m_CurrentVideo.cachetotal - DVD_MSEC_TO_TIME(1800);
+              clock = m_CurrentVideo.starttime - m_CurrentVideo.cachetotal;
           }
         }
       }
