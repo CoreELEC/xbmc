59ac2ca8a4cad24a8e12e9b9dfaadb728569aa00
afl1 <afl2001@gmail.com>
2019-05-19 21:00:58 +0200

    AMLCodec: update interlaced flag

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.cpp
@@ -2207,6 +2207,8 @@ float CAMLCodec::GetTimeSize()
 
 CDVDVideoCodec::VCReturn CAMLCodec::GetPicture(VideoPicture *pVideoPicture)
 {
+  std::string vfmt;
+
   if (!m_opened)
     return CDVDVideoCodec::VC_ERROR;
 
@@ -2235,6 +2237,10 @@ CDVDVideoCodec::VCReturn CAMLCodec::GetPicture(VideoPicture *pVideoPicture)
     return CDVDVideoCodec::VC_EOF;
   else if (timesize < 1.0)
     return CDVDVideoCodec::VC_BUFFER;
+
+  SysfsUtils::GetString("/sys/class/deinterlace/di0/frame_format", vfmt);
+  if (vfmt.c_str())
+    m_processInfo.SetVideoInterlaced(vfmt.compare("progressive"));
 
   return CDVDVideoCodec::VC_NONE;
 }
