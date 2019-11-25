3f74ce1bcba7e05b4055eac3d6b4c39dde995867
Arthur Liberman <arthur_liberman@hotmail.com>
2019-11-25 20:04:39 +0200

    AMLUtils: make disabling gui scaling optional

--- xbmc/settings/Settings.h
+++ xbmc/settings/Settings.h
@@ -419,6 +419,7 @@ public:
   static constexpr auto SETTING_COREELEC_AMLOGIC_HDR2SDR = "coreelec.amlogic.hdr2sdr";
   static constexpr auto SETTING_COREELEC_AMLOGIC_LIMIT8BIT = "coreelec.amlogic.limit8bit";
   static constexpr auto SETTING_COREELEC_AMLOGIC_FORCE422 = "coreelec.amlogic.force422";
+  static constexpr auto SETTING_COREELEC_AMLOGIC_DISABLEGUISCALING = "coreelec.amlogic.disableguiscaling";
   static constexpr auto SETTING_CACHE_HARDDISK = "cache.harddisk";
   static constexpr auto SETTING_CACHEVIDEO_DVDROM = "cachevideo.dvdrom";
   static constexpr auto SETTING_CACHEVIDEO_LAN = "cachevideo.lan";
