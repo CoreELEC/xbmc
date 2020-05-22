392cabc88cfff2f26ff092ad831e03c891956d25
Arthur Liberman <arthur_liberman@hotmail.com>
2020-05-23 01:28:55 +0300

    Revert "[videoplayer] Fix TS Streams by disallowing streaminfo for realtime streams"
    This reverts commit b2f110329003bd8d135194897b36a388651102ec.

--- xbmc/utils/URIUtils.cpp
+++ xbmc/utils/URIUtils.cpp
@@ -1791,3 +1791,8 @@ CURL URIUtils::AddCredentials(CURL url)
     CPasswordManager::GetInstance().AuthenticateURL(url);
   return url;
 }
+
+bool URIUtils::IsUsingFastSwitch(const std::string& strFile)
+{
+  return IsUDP(strFile) || IsTCP(strFile) || IsPVRChannel(strFile);
+}
