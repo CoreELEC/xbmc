9a35a1db6e8d6d2028a59e43730fe0c90a6a26b2
Raybuntu <57228613+Raybuntu@users.noreply.github.com>
2020-07-11 10:20:00 +0200

    WinSystemAmlogic: Only set frambuffer to hardcoded resolution on GXL and below. This is only needed for devices with the mali bug.

--- xbmc/windowing/amlogic/WinSystemAmlogic.cpp
+++ xbmc/windowing/amlogic/WinSystemAmlogic.cpp
@@ -144,7 +144,8 @@ bool CWinSystemAmlogic::InitWindowSystem()
   RETRO::CRPProcessInfoAmlogic::RegisterRendererFactory(new RETRO::CRendererFactoryOpenGLES);
   CRendererAML::Register();
 
-  aml_set_framebuffer_resolution(1920, 1080, m_framebuffer_name);
+  if (aml_get_cpufamily_id() <= AML_GXL)
+    aml_set_framebuffer_resolution(1920, 1080, m_framebuffer_name);
 
   return CWinSystemBase::InitWindowSystem();
 }
