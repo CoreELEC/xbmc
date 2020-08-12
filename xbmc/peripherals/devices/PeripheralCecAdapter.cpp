1aba23b16e686dc3e617afdf0d4f027491afea99
Raybuntu <57228613+Raybuntu@users.noreply.github.com>
2020-08-12 14:40:00 +0100

    PeripheralCecAdapter: Don't flood TV with isActiveSource requests

--- xbmc/peripherals/devices/PeripheralCecAdapter.cpp
+++ xbmc/peripherals/devices/PeripheralCecAdapter.cpp
@@ -1987,7 +1987,12 @@ void CPeripheralCecAdapter::ProcessStandbyDevices(void)
     bStandby = m_bStandbyPending;
     m_bStandbyPending = false;
     if (bStandby)
-      m_bGoingToStandby = true;
+    {
+      if (m_cecAdapter->IsLibCECActiveSource())
+        m_bGoingToStandby = true;
+      else
+        bStandby = false;
+    }
   }
 
   if (bStandby)
