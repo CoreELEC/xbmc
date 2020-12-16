e190eab60ced88d52f35cd49f1ff3765d6f2e133
Portisch <hugo.portisch@yahoo.de>
2020-12-16 12:46:09 +0100

    PeripheralCecAdapter: rework the suspend delay timer Change suspend delay from seconds to minutes.

--- xbmc/peripherals/devices/PeripheralCecAdapter.cpp
+++ xbmc/peripherals/devices/PeripheralCecAdapter.cpp
@@ -176,18 +176,15 @@ void CPeripheralCecAdapter::Announce(ANNOUNCEMENT::AnnouncementFlag flag,
     if (m_bPowerOffScreensaver)
     {
       // Don't put devices to standby if application is currently playing. Check if video is paused.
-      bool bNotPlaying = false;
       if (m_bPowerOffScreensaverPaused)
-        bNotPlaying = (!appPlayer->IsPlaying() || appPlayer->IsPausedPlayback());
-      else
-        bNotPlaying = (!appPlayer->IsPlaying());
-      if (bNotPlaying)
       {
-        // only power off when we're the active source
-        if (m_cecAdapter->IsLibCECActiveSource()) {
-          m_ScreensaverStandbySent = CDateTime::GetCurrentDateTime();
+        if (!appPlayer->IsPlaying() || appPlayer->IsPausedPlayback())
           StandbyDevices();
-        }
+      }
+      else
+      {
+        if (!appPlayer->IsPlaying())
+          StandbyDevices();
       }
     }
   }
@@ -1812,30 +1809,28 @@ void CPeripheralCecAdapter::StandbyDevices(void)
 {
   std::unique_lock<CCriticalSection> lock(m_critSection);
   m_bStandbyPending = true;
+  m_ScreensaverStandbySent = CDateTime::GetCurrentDateTime();
 }
 
 void CPeripheralCecAdapter::ProcessStandbyDevices(void)
 {
-  bool bStandby(false);
-
+  std::unique_lock<CCriticalSection> lock(m_critSection);
+  if (m_bStandbyPending)
   {
-    std::unique_lock<CCriticalSection> lock(m_critSection);
     int iScreensaverDelay = GetSettingInt("screensaver_delay_standby");
-    if ((m_ScreensaverStandbySent.IsValid() &&
-        (CDateTime::GetCurrentDateTime() - m_ScreensaverStandbySent < CDateTimeSpan(0, 0, 0, iScreensaverDelay))))
-      return;
-    bStandby = m_bStandbyPending;
+
+    if ((iScreensaverDelay > 0) && m_ScreensaverStandbySent.IsValid())
+      if (CDateTime::GetCurrentDateTime() - m_ScreensaverStandbySent < CDateTimeSpan(0, 0, iScreensaverDelay, 0))
+          return;
+
     m_bStandbyPending = false;
     m_ScreensaverStandbySent.SetValid(false);
-    if (bStandby)
-      if (m_cecAdapter->IsLibCECActiveSource())
-        m_bGoingToStandby = true;
-      else
-        bStandby = false;
-  }
 
-  if (bStandby)
-  {
+    if (!m_cecAdapter->IsLibCECActiveSource())
+      return;
+
+    m_bGoingToStandby = true;
+
     if (!m_configuration.powerOffDevices.IsEmpty())
     {
       m_standbySent = CDateTime::GetCurrentDateTime();
