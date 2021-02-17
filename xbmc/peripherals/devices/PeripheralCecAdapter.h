6c6f3cc596afa4e3b134a096730ef86e20d2af03
Portisch <hugo.portisch@yahoo.de>
2021-02-17 07:58:47 +0100

    PeripheralCecAdapter: Add CEC WakeUp configuration

--- xbmc/peripherals/devices/PeripheralCecAdapter.h
+++ xbmc/peripherals/devices/PeripheralCecAdapter.h
@@ -88,6 +88,15 @@ typedef enum
   VOLUME_CHANGE_MUTE
 } CecVolumeChange;
 
+// CEC Wake Up flags from u-boot(bl301)
+typedef enum
+{
+  CEC_FUNC_MASK = 0,
+  AUTO_POWER_ON_MASK = 3,
+  STREAMPATH_POWER_ON_MASK = 4,
+  ACTIVE_SOURCE_MASK = 6
+} CecWakeUpMask;
+
 class CPeripheralCecAdapter : public CPeripheralHID,
                               public KODI::CEC::ICecInputProvider,
                               public ANNOUNCEMENT::IAnnouncer,
@@ -211,6 +220,7 @@ private:
   bool m_bSendInactiveSource;
   bool m_bPowerOffScreensaver;
   bool m_bShutdownOnStandby;
+  int m_iCec_func_config;
 };
 
 class CPeripheralCecAdapterUpdateThread : public CThread
