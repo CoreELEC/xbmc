41e4c87745a42752ce280b9405f29ecffdc859aa
kszaq <737984+kszaq@users.noreply.github.com>
2021-05-03 23:23:04 +0200

    InputStreamAddon, DVDDemuxClient: assume progressive streams

--- xbmc/cores/VideoPlayer/DVDInputStreams/InputStreamAddon.cpp
+++ xbmc/cores/VideoPlayer/DVDInputStreams/InputStreamAddon.cpp
@@ -419,6 +419,8 @@ KODI_HANDLE CInputStreamAddon::cb_get_stream_transfer(KODI_HANDLE handle,
     videoStream->iHeight = stream->m_Height;
     videoStream->fAspect = stream->m_Aspect;
     videoStream->iBitRate = stream->m_BitRate;
+    videoStream->bUnknownIP = false;
+    videoStream->bInterlaced = false;
     videoStream->profile = ConvertVideoCodecProfile(stream->m_codecProfile);
 
     /*! Added on API version 2.0.8 */
