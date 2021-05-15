618e11790162bf2081d0199c7884237d72fb551d
Dmitry_L <freebox_2002@mail.ru>
2021-05-15 17:40:03 +0300

    VideoPlayerVideo: Fix interlace/progressive info in PlayerDebug
    It takes some time for correct HW di frame_format value to be in file.
    Now interlace flag checked only 4 times with some period on stream start or after some stream change.
    Contains revert for 2 commits:
    https://github.com/CoreELEC/xbmc/commit/e6f7a0d214f05dbf183fceaea662b851316635b0
    https://github.com/CoreELEC/xbmc/commit/a655c4fc29700d384cba9ce277a922d50517ad41

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.cpp
@@ -2284,8 +2284,6 @@ float CAMLCodec::GetTimeSize()
 
 CDVDVideoCodec::VCReturn CAMLCodec::GetPicture(VideoPicture *pVideoPicture)
 {
-  std::string vfmt;
-
   if (!m_opened)
     return CDVDVideoCodec::VC_ERROR;
 
@@ -2314,9 +2312,6 @@ CDVDVideoCodec::VCReturn CAMLCodec::GetPicture(VideoPicture *pVideoPicture)
     return CDVDVideoCodec::VC_EOF;
   else if (timesize < 1.0)
     return CDVDVideoCodec::VC_BUFFER;
-
-  if (!SysfsUtils::GetString("/sys/class/deinterlace/di0/frame_format", vfmt) && (vfmt.size() > 4))
-    m_processInfo.SetVideoInterlaced(vfmt.compare("progressive"));
 
   return CDVDVideoCodec::VC_NONE;
 }
