3dfc548294a30e5cb53b371cd847f7d31d319938
Portisch <hugo.portisch@yahoo.de>
2021-06-14 16:44:18 +0200

    VideoPlayerVideo: Fix interlace/progressive info in  PlayerDebug
    It takes some time for correct HW di frame_format value to be in file.
    Now interlace flag checked only 4 times with some period on stream start or after some stream change.
    Contains revert for 2 commits:
    https://github.com/CoreELEC/xbmc/commit/e6f7a0d214f05dbf183fceaea662b851316635b0
    https://github.com/CoreELEC/xbmc/commit/a655c4fc29700d384cba9ce277a922d50517ad41

--- xbmc/cores/VideoPlayer/VideoPlayerVideo.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayerVideo.cpp
@@ -18,6 +18,7 @@
 #include "settings/SettingsComponent.h"
 #include "utils/MathUtils.h"
 #include "utils/log.h"
+#include "utils/SysfsUtils.h"
 #include "windowing/GraphicContext.h"
 #include "windowing/WinSystem.h"
 
@@ -335,6 +336,9 @@ void CVideoPlayerVideo::Process()
   int iDropDirective;
   bool onlyPrioMsgs = false;
 
+  std::string vfmt;
+  int vfmtCheckCount = 0;
+
   m_videoStats.Start();
   m_droppingStats.Reset();
   m_iDroppedFrames = 0;
@@ -374,6 +378,11 @@ void CVideoPlayerVideo::Process()
           m_picture.videoBuffer)
       {
         m_outputSate = OutputPicture(&m_picture);
+        if (m_processInfo.IsVideoHwDecoder())
+        {
+          vfmtCheckCount = 16;
+          CLog::Log(LOGDEBUG, "CVideoPlayerVideo - OUTPUT_AGAIN - vfmt, interlace should be checked.");
+        }
         if (m_outputSate == OUTPUT_AGAIN)
         {
           onlyPrioMsgs = true;
@@ -444,6 +453,11 @@ void CVideoPlayerVideo::Process()
       m_renderManager.ShowVideo(true);
 
       CLog::Log(LOGDEBUG, "CVideoPlayerVideo - CDVDMsg::GENERAL_RESYNC({:f})", pts);
+      if (m_processInfo.IsVideoHwDecoder())
+      {
+        vfmtCheckCount = 16;
+        CLog::Log(LOGDEBUG, "CVideoPlayerVideo - OUTPUT_AGAIN - vfmt, interlace should be checked.");
+      }
     }
     else if (pMsg->IsType(CDVDMsg::VIDEO_SET_ASPECT))
     {
@@ -608,6 +622,13 @@ void CVideoPlayerVideo::Process()
         if (ProcessDecoderOutput(frametime, pts))
         {
           onlyPrioMsgs = true;
+        }
+
+        if (vfmtCheckCount > 0 && --vfmtCheckCount % 5 == 0)
+        {
+          if (!SysfsUtils::GetString("/sys/class/deinterlace/di0/frame_format", vfmt) && (vfmt.size() > 4))
+            m_processInfo.SetVideoInterlaced(vfmt.compare("progressive"));
+          CLog::Log(LOGDEBUG, "CVideoPlayerVideo - CDVDMsg::DEMUXER_PACKET - checking interlace vfmt: {}", vfmt);
         }
       }
       else
