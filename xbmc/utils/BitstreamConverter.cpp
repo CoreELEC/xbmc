0bebf35a4f32d93879a0e0ffc9a88bc31da2c521
Portisch <hugo.portisch@yahoo.de>
2021-06-14 15:42:26 +0200

    VideoPlayer: SD channels assume 16:9
    Assume 16:9 aspect ratio for SD at playback start.
    Real aspec ratio will be fix later from stream.
    
    Patch: kodi-le-037-SD-channels-assume-16-9.patch

--- xbmc/utils/BitstreamConverter.cpp
+++ xbmc/utils/BitstreamConverter.cpp
@@ -97,7 +97,7 @@ enum {
  *  Copyright (C) 2005 Michal Benes <michal.benes@itonis.tv>
  *            (C) 2008 Wim Taymans <wim.taymans@gmail.com>
  *  gsth264parse.c
- *  
+ *
  *  SPDX-License-Identifier: LGPL-2.1-or-later
  *  See LICENSES/README.md for more information.
  */
@@ -1464,7 +1464,7 @@ bool CBitstreamConverter::h264_sequence_header(const uint8_t *data, const uint32
                     break;
                 case 4:
                     // 16:11
-                    ratio *= 1.4545454545454546;
+                    ratio *= 1.4222222222222222;
                     break;
                 case 5:
                     // 40:33
@@ -1527,6 +1527,11 @@ bool CBitstreamConverter::h264_sequence_header(const uint8_t *data, const uint32
                 changed = true;
                 sequence->ratio = ratio;
                 sequence->ratio_info = aspect_ratio_idc;
+            }
+            if (changed)
+            {
+              CLog::Log(LOGDEBUG, "CBitstreamConverter::h264_sequence_header: "
+                "width({:d}), height({:d}), ratio(%f), {:d}x{:d}", pic_width_cropped, pic_height_cropped, ratio, sar_width, sar_height);
             }
 
             break;
