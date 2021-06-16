51f9671150ab45e6e9761e290b4fc0d5876b1700
Portisch <hugo.portisch@yahoo.de>
2021-06-16 16:43:10 +0200

    DisplaySettings: save kernel_hdmimode instead hdmimode Do not mess up with bootloader 'hdmimode' environment variable. Save also all resolutions supported by the kernel.

--- xbmc/settings/DisplaySettings.cpp
+++ xbmc/settings/DisplaySettings.cpp
@@ -23,6 +23,7 @@
 #include "settings/lib/SettingDefinitions.h"
 #include "storage/MediaManager.h"
 #include "utils/StringUtils.h"
+#include "utils/SysfsUtils.h"
 #include "utils/Variant.h"
 #include "utils/XMLUtils.h"
 #include "utils/log.h"
@@ -95,11 +96,14 @@ static bool write_resolution_ini(RESOLUTION_INFO res)
 
   if (!result && !res.strId.empty())
   {
+    std::string allfmt_names = "";
+    SysfsUtils::GetString("/sys/class/amhdmitx/amhdmitx0/allfmt_names", allfmt_names);
     std::ofstream ofs(aml_res_path + "/" + aml_res_file, std::ofstream::out);
     ofs << "# WARNING DO NOT MODIFY THIS FILE! ALL CHANGES WILL BE LOST!\n";
-    ofs << "hdmimode=" << res.strId.c_str() << "\n";
+    ofs << "kernel_hdmimode=" << res.strId.c_str() << "\n";
     ofs << "frac_rate_policy=" << std::to_string((res.fRefreshRate == floor(res.fRefreshRate)) ? 0 : 1).c_str() << "\n";
     ofs << "native_4k_gui=" << std::to_string(nativeGui).c_str() << "\n";
+    ofs << "allfmt_names=" << allfmt_names.c_str() << "\n";
     ofs.close();
     CLog::Log(LOGDEBUG, "CDisplaySettings: Amlogic resolution got saved to {}/{}", aml_res_path.c_str(), aml_res_file.c_str());
   }
