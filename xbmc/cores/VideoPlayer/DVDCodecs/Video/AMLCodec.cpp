a7058c515852fac45d71d963f7fc135954cf1e37
kszaq <737984+kszaq@users.noreply.github.com>
2023-01-09 23:30:49 +0100

    AMLCodec: reset m_last_pts on Reset() (e.g. seek)

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.cpp
@@ -2274,6 +2274,7 @@ void CAMLCodec::Reset()
 
   // reset some interal vars
   m_cur_pts = DVD_NOPTS_VALUE;
+  m_last_pts = DVD_NOPTS_VALUE;
   m_state = 0;
   m_frameSizes.clear();
   m_frameSizeSum = 0;
