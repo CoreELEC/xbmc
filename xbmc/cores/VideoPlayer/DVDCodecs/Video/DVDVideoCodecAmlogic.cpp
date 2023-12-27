5318828149b8d4a96d660e513c2850ef8c771734
Arthur Liberman <arthur_liberman@hotmail.com>
2023-12-28 00:44:36 +0200

    AMLCodec: Restart codec in `dec_mode=STREAM_TYPE_FRAME` on DoVi P7 MEL Dolby Vision Profile 7 Minimum Enhancement Layer (MEL) can't use `STREAM_TYPE_STREAM` decoder mode. The kernel can detect MEL content, it sets `is_mel=true` in sysfs, we detect it, and restart the codec in the correct mode

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAmlogic.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAmlogic.cpp
@@ -436,6 +436,12 @@ void CDVDVideoCodecAmlogic::Reset(void)
     m_bitstream->ResetStartDecode();
 }
 
+void CDVDVideoCodecAmlogic::Reopen(void)
+{
+  if (m_Codec && !m_Codec->OpenDecoder(m_hints))
+    CLog::Log(LOGERROR, "{}: Failed to reopen Amlogic Codec", __MODULE_NAME__);
+}
+
 CDVDVideoCodec::VCReturn CDVDVideoCodecAmlogic::GetPicture(VideoPicture* pVideoPicture)
 {
   if (!m_Codec)
@@ -443,7 +449,11 @@ CDVDVideoCodec::VCReturn CDVDVideoCodecAmlogic::GetPicture(VideoPicture* pVideoP
 
   VCReturn retVal = m_Codec->GetPicture(&m_videobuffer);
 
-  if (retVal == VC_PICTURE)
+  if (retVal == VC_REOPEN)
+  {
+    m_Codec->CloseDecoder();
+  }
+  else if (retVal == VC_PICTURE)
   {
     pVideoPicture->SetParams(m_videobuffer);
 
