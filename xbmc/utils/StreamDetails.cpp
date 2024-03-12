cfb8d5580a9229d801aa2fa81162748ee74ea3cc
Portisch <hugo.portisch@yahoo.de>
2024-03-12 19:44:29 +0100

    Rework Kodi force switch algorithms. Forced mode switch detection by cd/cs is not 100% reliable.
    Because of Dolby Vision it is possible resolution and frequency do not change.
    But if colour subsampling or colour depth is changed a forced mode switch is required.
    Force mode switch by set mode 'null'.
    In special cases a hard mode reset is needed like 4k420 to 4kDV.

--- xbmc/utils/StreamDetails.cpp
+++ xbmc/utils/StreamDetails.cpp
@@ -669,6 +669,6 @@ std::string CStreamDetails::HdrTypeToString(StreamHdrType hdrType)
       return "hlg";
     case StreamHdrType::HDR_TYPE_NONE:
     default:
-      return "";
+      return "sdr";
   }
 }
