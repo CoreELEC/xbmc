78b17d40fb892e4bd3bb6b06d8d40bf67843343d
Portisch <hugo.portisch@yahoo.de>
2024-03-12 19:44:29 +0100

    Rework Kodi force switch algorithms. Forced mode switch detection by cd/cs is not 100% reliable.
    Because of Dolby Vision it is possible resolution and frequency do not change.
    But if colour subsampling or colour depth is changed a forced mode switch is required.
    Force mode switch by set mode 'null'.
    In special cases a hard mode reset is needed like 4k420 to 4kDV.

--- xbmc/windowing/amlogic/WinSystemAmlogicGLESContext.cpp
+++ xbmc/windowing/amlogic/WinSystemAmlogicGLESContext.cpp
@@ -18,12 +18,6 @@
 using namespace KODI;
 using namespace KODI::WINDOWING::AML;
 
-CWinSystemAmlogicGLESContext::CWinSystemAmlogicGLESContext()
-:  m_cs(-1)
-,  m_cd(-1)
-{
-}
-
 void CWinSystemAmlogicGLESContext::Register()
 {
   KODI::WINDOWING::CWindowSystemFactory::RegisterWindowSystem(CreateWinSystem, "aml");
@@ -94,16 +88,11 @@ bool CWinSystemAmlogicGLESContext::CreateNewWindow(const std::string& name,
       amhdmitx0_frac_rate_policy.Set(fractional_rate);
   }
 
-  // check for colour subsampling/depth change
-  CSysfsPath amhdmitx0_cs{"/sys/class/amhdmitx/amhdmitx0/cs"};
-  CSysfsPath amhdmitx0_cd{"/sys/class/amhdmitx/amhdmitx0/cd"};
-  int cs = 0;
-  int cd = 0;
-  if (amhdmitx0_cs.Exists() && amhdmitx0_cd.Exists())
-  {
-    cs = amhdmitx0_cs.Get<int>().value();
-    cd = amhdmitx0_cd.Get<int>().value();
-  }
+  StreamHdrType hdrType = CServiceBroker::GetWinSystem()->GetGfxContext().GetHDRType();
+  bool force_mode_switch_by_dv = false;
+  if ((m_hdrType == StreamHdrType::HDR_TYPE_DOLBYVISION && hdrType != StreamHdrType::HDR_TYPE_DOLBYVISION) ||
+      (m_hdrType != StreamHdrType::HDR_TYPE_DOLBYVISION && hdrType == StreamHdrType::HDR_TYPE_DOLBYVISION))
+      force_mode_switch_by_dv = true;
 
   // get current used resolution
   if (!aml_get_native_resolution(&current_resolution))
@@ -112,14 +101,16 @@ bool CWinSystemAmlogicGLESContext::CreateNewWindow(const std::string& name,
     return false;
   }
 
+  const std::string new_hdrStr = CStreamDetails::HdrTypeToString(hdrType);
+  const std::string old_hdrStr = CStreamDetails::HdrTypeToString(m_hdrType);
   CLog::Log(LOGDEBUG, "CWinSystemAmlogicGLESContext::{}: "
     "m_bWindowCreated: {}, "
     "frac rate {:d}({:d}), "
-    "cs: {:d}({:d}), cd: {:d}({:d})",
+    "hdrType: {}({}), force mode switch: {}",
     __FUNCTION__,
     m_bWindowCreated,
     fractional_rate, cur_fractional_rate,
-    cs, m_cs, cd, m_cd);
+    new_hdrStr.empty() ? "none" : new_hdrStr, old_hdrStr.empty() ? "none" : old_hdrStr, force_mode_switch_by_dv);
   CLog::Log(LOGDEBUG, "CWinSystemAmlogicGLESContext::{}: "
     "cur: iWidth: {:04d}, iHeight: {:04d}, iScreenWidth: {:04d}, iScreenHeight: {:04d}, fRefreshRate: {:02.2f}, dwFlags: {:02x}",
     __FUNCTION__,
@@ -136,7 +127,7 @@ bool CWinSystemAmlogicGLESContext::CreateNewWindow(const std::string& name,
       m_bFullScreen == fullScreen && current_resolution.fRefreshRate == res.fRefreshRate &&
       (current_resolution.dwFlags & D3DPRESENTFLAG_MODEMASK) == (res.dwFlags & D3DPRESENTFLAG_MODEMASK) &&
       m_stereo_mode == stereo_mode && m_bWindowCreated &&
-      ((m_cs != -1 && m_cd != -1) && (m_cs == cs && m_cd == cd)) &&
+      !force_mode_switch_by_dv &&
       (fractional_rate == cur_fractional_rate))
   {
     CLog::Log(LOGDEBUG, "CWinSystemAmlogicGLESContext::{}: No need to create a new window", __FUNCTION__);
@@ -150,7 +141,7 @@ bool CWinSystemAmlogicGLESContext::CreateNewWindow(const std::string& name,
   if ((current_resolution.iWidth == res.iWidth && current_resolution.iHeight == res.iHeight &&
        current_resolution.iScreenWidth == res.iScreenWidth && current_resolution.iScreenHeight == res.iScreenHeight &&
        current_resolution.fRefreshRate == res.fRefreshRate) &&
-       (((m_cs != -1 && m_cd != -1) && (m_cs != cs || m_cd != cd)) ||
+       (force_mode_switch_by_dv ||
        (fractional_rate != cur_fractional_rate)))
   {
     m_force_mode_switch = true;
@@ -181,12 +172,7 @@ bool CWinSystemAmlogicGLESContext::CreateNewWindow(const std::string& name,
   }
 
   // backup data after mode switch
-  if (amhdmitx0_cs.Exists() && amhdmitx0_cd.Exists())
-  {
-    m_cs = cs;
-    m_cd = cd;
-  }
-
+  m_hdrType = hdrType;
   m_stereo_mode = stereo_mode;
   m_bFullScreen = fullScreen;
 
