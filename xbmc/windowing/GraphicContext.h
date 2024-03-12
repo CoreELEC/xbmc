3d3675e0547bd6b47dd8f63e943b24b291c85fcf
Portisch <hugo.portisch@yahoo.de>
2024-03-12 19:44:29 +0100

    Rework Kodi force switch algorithms. Forced mode switch detection by cd/cs is not 100% reliable.
    Because of Dolby Vision it is possible resolution and frequency do not change.
    But if colour subsampling or colour depth is changed a forced mode switch is required.
    Force mode switch by set mode 'null'.
    In special cases a hard mode reset is needed like 4k420 to 4kDV.

--- xbmc/windowing/GraphicContext.h
+++ xbmc/windowing/GraphicContext.h
@@ -13,6 +13,7 @@
 #include "threads/CriticalSection.h"
 #include "utils/ColorUtils.h"
 #include "utils/Geometry.h" // for CRect/CPoint
+#include "utils/StreamDetails.h"
 #include "utils/TransformMatrix.h" // for the members m_guiTransform etc.
 
 #include <map>
@@ -163,6 +164,8 @@ public:
   RENDER_STEREO_VIEW GetStereoView()  { return m_stereoView; }
   void SetStereoMode(RENDER_STEREO_MODE mode) { m_nextStereoMode = mode; }
   RENDER_STEREO_MODE GetStereoMode()  { return m_stereoMode; }
+  void SetHDRType(StreamHdrType hdrType)  { m_hdrType = hdrType; }
+  StreamHdrType GetHDRType()  { return m_hdrType; }
   void RestoreCameraPosition();
   void SetStereoFactor(float factor);
   void RestoreStereoFactor();
@@ -274,6 +277,7 @@ protected:
   RENDER_STEREO_VIEW m_stereoView = RENDER_STEREO_VIEW_OFF;
   RENDER_STEREO_MODE m_stereoMode = RENDER_STEREO_MODE_OFF;
   RENDER_STEREO_MODE m_nextStereoMode = RENDER_STEREO_MODE_OFF;
+  StreamHdrType m_hdrType;
 
   bool m_isTransferPQ{false};
   RENDER_ORDER m_renderOrder{RENDER_ORDER_ALL_BACK_TO_FRONT};
