759db2d202dbf7e1a08ea5f7d41e7f9814985cef
Portisch <hugo.portisch@yahoo.de>
2024-04-23 14:22:08 +0200

    AML: wait until kernel started playback Wait maximal 1000ms before perform a mode switch. Especially for DV it's need wait till playback is started.

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
@@ -21,6 +21,7 @@
 #include "settings/Settings.h"
 #include "settings/SettingsComponent.h"
 #include "threads/SingleLock.h"
+#include "utils/AMLUtils.h"
 #include "utils/StreamDetails.h"
 #include "utils/StringUtils.h"
 #include "utils/XTimeUtils.h"
@@ -218,6 +219,7 @@ bool CRenderManager::Configure()
       m_free.push_back(i);
 
     m_bRenderGUI = true;
+    m_videostarted = std::chrono::system_clock::now();
     m_bTriggerUpdateResolution = true;
     m_presentstep = PRESENT_IDLE;
     m_presentpts = DVD_NOPTS_VALUE;
@@ -898,18 +900,22 @@ void CRenderManager::UpdateResolution()
   {
     if (CServiceBroker::GetWinSystem()->GetGfxContext().IsFullScreenVideo() && CServiceBroker::GetWinSystem()->GetGfxContext().IsFullScreenRoot())
     {
-      if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetInt(CSettings::SETTING_VIDEOPLAYER_ADJUSTREFRESHRATE) != ADJUST_REFRESHRATE_OFF && m_fps > 0.0f)
+      auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - m_videostarted);
+      if (aml_video_started() || elapsed > 1000ms)
       {
-        RESOLUTION res = CResolutionUtils::ChooseBestResolution(
-            m_fps, m_picture.iWidth, m_picture.iHeight, !m_picture.stereoMode.empty());
-        CServiceBroker::GetWinSystem()->GetGfxContext().SetHDRType(m_picture.hdrType);
-        CServiceBroker::GetWinSystem()->GetGfxContext().SetVideoResolution(res, false);
-        UpdateLatencyTweak();
-        if (m_pRenderer)
-          m_pRenderer->Update();
+        if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetInt(CSettings::SETTING_VIDEOPLAYER_ADJUSTREFRESHRATE) != ADJUST_REFRESHRATE_OFF && m_fps > 0.0f)
+        {
+          RESOLUTION res = CResolutionUtils::ChooseBestResolution(
+              m_fps, m_picture.iWidth, m_picture.iHeight, !m_picture.stereoMode.empty());
+          CServiceBroker::GetWinSystem()->GetGfxContext().SetHDRType(m_picture.hdrType);
+          CServiceBroker::GetWinSystem()->GetGfxContext().SetVideoResolution(res, false);
+          UpdateLatencyTweak();
+          if (m_pRenderer)
+            m_pRenderer->Update();
+        }
+        m_bTriggerUpdateResolution = false;
+        m_playerPort->VideoParamsChange();
       }
-      m_bTriggerUpdateResolution = false;
-      m_playerPort->VideoParamsChange();
     }
   }
 }
@@ -923,6 +929,7 @@ void CRenderManager::TriggerUpdateResolution(float fps, int width, int height, s
     m_picture.iHeight = height;
     m_picture.stereoMode = stereomode;
   }
+  m_videostarted = std::chrono::system_clock::now();
   m_bTriggerUpdateResolution = true;
 }
 
