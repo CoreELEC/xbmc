8c1b0665f0389f3723b55ef2d20fa80131dd2f81
Portisch <hugo.portisch@yahoo.de>
2024-04-23 14:22:08 +0200

    AML: wait until kernel started playback Wait maximal 1000ms before perform a mode switch. Hide all frames on screen until mode switch was done.

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
@@ -21,6 +21,7 @@
 #include "settings/Settings.h"
 #include "settings/SettingsComponent.h"
 #include "threads/SingleLock.h"
+#include "utils/AMLUtils.h"
 #include "utils/StreamDetails.h"
 #include "utils/StringUtils.h"
 #include "utils/XTimeUtils.h"
@@ -209,6 +210,7 @@ bool CRenderManager::Configure()
       m_free.push_back(i);
 
     m_bRenderGUI = true;
+    m_videostarted = std::chrono::system_clock::now();
     m_bTriggerUpdateResolution = true;
     m_presentstep = PRESENT_IDLE;
     m_presentpts = DVD_NOPTS_VALUE;
@@ -886,18 +888,22 @@ void CRenderManager::UpdateResolution()
   {
     if (CServiceBroker::GetWinSystem()->GetGfxContext().IsFullScreenVideo() && CServiceBroker::GetWinSystem()->GetGfxContext().IsFullScreenRoot())
     {
-      if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetInt(CSettings::SETTING_VIDEOPLAYER_ADJUSTREFRESHRATE) != ADJUST_REFRESHRATE_OFF && m_fps > 0.0f)
+      auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - m_videostarted);
+      if (aml_video_started() || elapsed > 1000ms)
       {
-        RESOLUTION res = CResolutionUtils::ChooseBestResolution(
-            m_fps, m_picture.iWidth, m_picture.iHeight, !m_picture.stereoMode.empty());
-        CServiceBroker::GetWinSystem()->GetGfxContext().SetHDRType(m_picture.hdrType);
-        CServiceBroker::GetWinSystem()->GetGfxContext().SetVideoResolution(res, false);
-        UpdateLatencyTweak();
-        if (m_pRenderer)
-          m_pRenderer->Update();
+        if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetInt(CSettings::SETTING_VIDEOPLAYER_ADJUSTREFRESHRATE) != ADJUST_REFRESHRATE_OFF && m_fps > 0.0f)
+        {
+          RESOLUTION res = CResolutionUtils::ChooseBestResolution(
+              m_fps, m_picture.iWidth, m_picture.iHeight, !m_picture.stereoMode.empty());
+          CServiceBroker::GetWinSystem()->GetGfxContext().SetHDRType(m_picture.hdrType);
+          CServiceBroker::GetWinSystem()->GetGfxContext().SetVideoResolution(res, false);
+          UpdateLatencyTweak();
+          if (m_pRenderer)
+            m_pRenderer->Update();
+        }
+        m_bTriggerUpdateResolution = false;
+        m_playerPort->VideoParamsChange();
       }
-      m_bTriggerUpdateResolution = false;
-      m_playerPort->VideoParamsChange();
     }
   }
 }
@@ -911,6 +917,7 @@ void CRenderManager::TriggerUpdateResolution(float fps, int width, int height, s
     m_picture.iHeight = height;
     m_picture.stereoMode = stereomode;
   }
+  m_videostarted = std::chrono::system_clock::now();
   m_bTriggerUpdateResolution = true;
 }
 
@@ -1181,6 +1188,9 @@ void CRenderManager::PrepareNextRender()
     m_presentsourcePast = -1;
     combined = true;
   }
+
+  //if (renderPts >= nextFramePts && !m_forceNext)
+  //  aml_video_mute(false);
 
   if (renderPts >= nextFramePts || m_forceNext)
   {
