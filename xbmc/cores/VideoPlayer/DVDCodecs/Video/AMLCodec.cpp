2ffefb682e27ad43e72782a8f60c6a831dde1d65
Portisch <hugo.portisch@yahoo.de>
2024-05-02 12:10:37 +0200

    AMLCodec: disable DV when display does not support it

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.cpp
@@ -2003,10 +2003,11 @@ bool CAMLCodec::OpenDecoder(CDVDStreamInfo &hints, enum ELType dovi_el_type)
   // enable Dolby Vision driver when 'dovi.ko' is available
   bool device_support_dv(aml_support_dolby_vision());
   bool user_dv_disable(CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_COREELEC_AMLOGIC_DV_DISABLE));
-  bool dv_enable(device_support_dv && !user_dv_disable && hints.hdrType == StreamHdrType::HDR_TYPE_DOLBYVISION);
-  CLog::Log(LOGINFO, "CAMLCodec::OpenDecoder Amlogic device {} support DV, DV is {} by user, DV system is {}",
+  bool dv_enable(device_support_dv && !user_dv_disable &&
+    hints.hdrType == StreamHdrType::HDR_TYPE_DOLBYVISION && aml_display_support_dv());
+  CLog::Log(LOGINFO, "CAMLCodec::OpenDecoder Amlogic device {} support DV, DV is {} by user, display {} support DV, DV system is {}",
     device_support_dv ? "does" : "does not", user_dv_disable ? "disabled" : "enabled",
-    dv_enable ? "enabled" : "disabled");
+    aml_display_support_dv() ? "does" : "does not", dv_enable ? "enabled" : "disabled");
   if (dv_enable)
   {
     // enable Dolby Vision
