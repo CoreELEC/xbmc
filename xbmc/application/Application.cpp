d40fd86278f1ac6aee3b61ab68ce714fc8cb3088
cpm <162544069+cpm-code@users.noreply.github.com>
2025-01-18 13:05:29 +0000

    Application: swap order of GUIWindowManager Render and RenderEx

--- xbmc/application/Application.cpp
+++ xbmc/application/Application.cpp
@@ -847,6 +847,9 @@ void CApplication::Render()
   if (!CServiceBroker::GetRenderSystem()->BeginRender())
     return;
 
+  // render video layer
+  CServiceBroker::GetGUI()->GetWindowManager().RenderEx();
+
   // render gui layer
   if (appPower->GetRenderGUI() && !m_skipGuiRender)
   {
@@ -871,9 +874,6 @@ void CApplication::Render()
 
     m_lastRenderTime = std::chrono::steady_clock::now();
   }
-
-  // render video layer
-  CServiceBroker::GetGUI()->GetWindowManager().RenderEx();
 
   CServiceBroker::GetRenderSystem()->EndRender();
 
