a286f4f150c8e8b8a001b1868676f2237d5d6e86
Portisch <hugo.portisch@yahoo.de>
2025-03-12 09:34:36 +0100

    HDRCapabilities: split Dolby Vision into 4k30Hz and 4k60Hz Some displays support Dolby Vision only up to 4k30Hz.

--- xbmc/guilib/guiinfo/SystemGUIInfo.cpp
+++ xbmc/guilib/guiinfo/SystemGUIInfo.cpp
@@ -323,8 +323,10 @@ bool CSystemGUIInfo::GetLabel(std::string& value, const CFileItem *item, int con
           types += ", HLG";
         if (caps.SupportsHDR10Plus())
           types += ", HDR10+";
-        if (caps.SupportsDolbyVision())
-          types += ", Dolby Vision";
+        if (caps.SupportsDolbyVision() == DolbyVisionFormat::DOLBYVISION_TYPE_4K30)
+          types += ", Dolby Vision up to 4k30Hz";
+        if (caps.SupportsDolbyVision() == DolbyVisionFormat::DOLBYVISION_TYPE_4K60)
+          types += ", Dolby Vision up to 4k60Hz";
 
         value = types;
       }
