aad488775b8f3be78dd87f827da91c3a997e4d2b
Portisch <hugo.portisch@yahoo.de>
2025-03-12 09:34:36 +0100

    HDRCapabilities: split Dolby Vision into 4k30Hz and 4k60Hz Some displays support Dolby Vision only up to 4k30Hz.

--- xbmc/utils/HDRCapabilities.h
+++ xbmc/utils/HDRCapabilities.h
@@ -8,6 +8,13 @@
 
 #pragma once
 
+enum class DolbyVisionFormat
+{
+  DOLBYVISION_TYPE_NONE,
+  DOLBYVISION_TYPE_4K30,
+  DOLBYVISION_TYPE_4K60
+};
+
 class CHDRCapabilities
 {
 public:
@@ -17,16 +24,17 @@ public:
   bool SupportsHDR10() const { return m_hdr10; }
   bool SupportsHLG() const { return m_hlg; }
   bool SupportsHDR10Plus() const { return m_hdr10_plus; }
-  bool SupportsDolbyVision() const { return m_dolby_vision; }
+  DolbyVisionFormat SupportsDolbyVision() const { return m_dolby_vision; }
 
   void SetHDR10() { m_hdr10 = true; }
   void SetHLG() { m_hlg = true; }
   void SetHDR10Plus() { m_hdr10_plus = true; }
-  void SetDolbyVision() { m_dolby_vision = true; }
+  void SetDolbyVision() { m_dolby_vision = DolbyVisionFormat::DOLBYVISION_TYPE_4K30; }
+  void SetDolbyVision4k60() { m_dolby_vision = DolbyVisionFormat::DOLBYVISION_TYPE_4K60; }
 
 private:
   bool m_hdr10 = false;
   bool m_hlg = false;
   bool m_hdr10_plus = false;
-  bool m_dolby_vision = false;
+  DolbyVisionFormat m_dolby_vision = DolbyVisionFormat::DOLBYVISION_TYPE_NONE;
 };
