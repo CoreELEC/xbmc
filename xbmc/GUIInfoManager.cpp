53ffe7f59adfac3bb467b54a246fb7223fa69028
Portisch <hugo.portisch@yahoo.de>
2025-03-26 17:00:00 +0100

    alsa: provide ALSA output channel layout Show audio input and output layout in process info.

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -1168,7 +1168,7 @@ constexpr std::array<InfoMap, 10> player_times = {{
 ///
 /// -----------------------------------------------------------------------------
 // clang-format off
-constexpr std::array<InfoMap, 16> player_process = {{
+constexpr std::array<InfoMap, 17> player_process = {{
     {"videodecoder",        PLAYER_PROCESS_VIDEODECODER},
     {"deintmethod",         PLAYER_PROCESS_DEINTMETHOD},
     {"pixformat",           PLAYER_PROCESS_PIXELFORMAT},
@@ -1185,6 +1185,7 @@ constexpr std::array<InfoMap, 16> player_process = {{
     {"amlogic.pixformat",   PLAYER_PROCESS_AML_PIXELFORMAT},
     {"amlogic.displaymode", PLAYER_PROCESS_AML_DISPLAYMODE},
     {"amlogic.eoft_gamut",  PLAYER_PROCESS_AML_EOFT_GAMUT},
+    {"audiochannelssink",   PLAYER_PROCESS_AUDIOCHANNELS_SINK},
 }};
 // clang-format on
 
