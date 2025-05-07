165e90dee91d93a0e3d531e00deea98d204b5ca7
Portisch <hugo.portisch@yahoo.de>
2025-05-07 15:47:04 +0200

    Application: cancel all library jobs on sleep

--- xbmc/application/Application.cpp
+++ xbmc/application/Application.cpp
@@ -1810,11 +1810,7 @@ bool CApplication::Stop(int exitCode)
     CServiceBroker::GetJobManager()->CancelJobs();
 
     // stop scanning before we kill the network and so on
-    if (CMusicLibraryQueue::GetInstance().IsRunning())
-      CMusicLibraryQueue::GetInstance().CancelAllJobs();
-
-    if (CVideoLibraryQueue::GetInstance().IsRunning())
-      CVideoLibraryQueue::GetInstance().CancelAllJobs();
+    CancelUpdateLibraries();
 
     CServiceBroker::GetAppMessenger()->Cleanup();
 
@@ -2685,6 +2681,21 @@ void CApplication::UpdateLibraries()
     CMusicLibraryQueue::GetInstance().ScanLibrary(
         "", MUSIC_INFO::CMusicInfoScanner::SCAN_NORMAL,
         !settings->GetBool(CSettings::SETTING_MUSICLIBRARY_BACKGROUNDUPDATE));
+  }
+}
+
+void CApplication::CancelUpdateLibraries()
+{
+  if (CMusicLibraryQueue::GetInstance().IsRunning())
+  {
+    CLog::LogF(LOGINFO, "Cancel music library scan");
+    CMusicLibraryQueue::GetInstance().CancelAllJobs();
+  }
+
+  if (CVideoLibraryQueue::GetInstance().IsRunning())
+  {
+    CLog::LogF(LOGINFO, "Cancel video library scan");
+    CVideoLibraryQueue::GetInstance().CancelAllJobs();
   }
 }
 
