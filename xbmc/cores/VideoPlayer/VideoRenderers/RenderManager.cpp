6e7b7caeb72f03e889e26426e7a06f0a2f322ff9
Portisch <hugo.portisch@yahoo.de>
2025-05-12 15:20:29 +0200

    Render: clear skipped frames on display mode change Frames are skipped until TV do mode change. The frames are not visible at all.

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
@@ -1252,6 +1252,13 @@ void CRenderManager::PrepareNextRender()
       }
     }
 
+    if (m_displayReset)
+    {
+      m_QueueSkip = 0;
+      m_lateframes = 0;
+      m_displayReset = false;
+    }
+
     if (lateframes)
       m_lateframes += lateframes;
     else
