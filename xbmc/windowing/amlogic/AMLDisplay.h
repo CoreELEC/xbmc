ea74263414e7cd70b043433cfaf7a9a08ec932c8
Portisch <hugo.portisch@yahoo.de>
2025-06-13 08:36:01 +0200

    AML Windowing: add support of GBM mali interface

--- xbmc/windowing/amlogic/AMLDisplay.h
+++ xbmc/windowing/amlogic/AMLDisplay.h
@@ -5,6 +5,7 @@
  *  See LICENSES/README.md for more information.
  */
 
+#include <gbm.h>
 #include <xf86drm.h>
 #include <xf86drmMode.h>
 #include <memory>
@@ -13,6 +14,34 @@
 #include "rendering/RenderSystemTypes.h"
 #include "threads/CriticalSection.h"
 #include "windowing/Resolution.h"
+
+struct drm_fb
+{
+  uint32_t fb_id;
+  uint32_t format;
+};
+
+class CAMLGBMUtils
+{
+public:
+  CAMLGBMUtils(int fd);
+  virtual ~CAMLGBMUtils();
+  struct gbm_device *GetDevice() const { return m_device; }
+  struct gbm_surface *GetSurface() const { return m_surface; }
+  bool CreateSurface(int width, int height, uint32_t format);
+  uint32_t GetFBId() { return m_drm_fb->fb_id; }
+  void LockFrontBuffer(int fd);
+  void UnlockFrontBuffer() { if (m_buffer)
+                               gbm_surface_release_buffer(m_surface, m_buffer); }
+private:
+  struct drm_fb* GetFBFromBo(int fd, struct gbm_bo* bo);
+
+  uint32_t m_format;
+  struct gbm_device *m_device;
+  struct gbm_surface *m_surface;
+  struct gbm_bo* m_buffer;
+  struct drm_fb* m_drm_fb;
+};
 
 class CAMLDRMUtils
 {
@@ -30,8 +59,10 @@ public:
   void aml_set_drmProperty(std::string name, unsigned int obj_type, unsigned int value);
   int aml_get_drmDevice_modes_count(drmModeConnection *connection);
   std::string aml_get_drmDevice_preferred_mode();
+  bool aml_set_drmDevice_active(std::string mode, bool active);
   bool aml_set_drmDevice_hotplug_mode(std::string mode);
   bool aml_get_drmDevice_connected() const { return m_connection == DRM_MODE_CONNECTED; }
+  void FlipPage(uint32_t fb_id);
 private:
   void CleanAndClose();
   void aml_init_drmDevice_display();
@@ -41,7 +72,12 @@ private:
   int get_drmProp(unsigned int id, std::string name, unsigned int obj_type);
   void set_drmProp(unsigned int id, std::string name,
     unsigned int obj_type, unsigned int value, drmModeAtomicReqPtr req);
+  bool SupportsFormat(drmModePlane *plane, uint32_t format);
   int m_fd{-1};
+  int m_width;
+  int m_height;
+  int m_ScreenWidth;
+  int m_ScreenHeight;
   CCriticalSection m_drmSection;
 
   drmModeResPtr m_resources{nullptr};
@@ -50,6 +86,7 @@ private:
   drmModeEncoderPtr m_encoder{nullptr};
   drmModeCrtcPtr m_crtc{nullptr};
   drmModeCrtcPtr m_orig_crtc{nullptr};
+  drmModePlanePtr m_plane{nullptr};
 };
 
 class CAMLDisplay
@@ -73,6 +110,9 @@ public:
   bool aml_probe_resolutions(std::vector<RESOLUTION_INFO> &resolutions);
   int aml_get_drmProperty(std::string name, unsigned int obj_type) const
     { return m_amlDRMUtils->aml_get_drmProperty(name, obj_type); }
+  void FlipPage(uint32_t fb_id) { m_amlDRMUtils->FlipPage(fb_id); }
+  bool aml_set_drmDevice_active(bool active) const
+    { return m_amlDRMUtils->aml_set_drmDevice_active(m_amlDRMUtils->aml_get_drmDevice_mode(), active); }
 private:
   std::unique_ptr<CAMLDRMUtils> m_amlDRMUtils;
   bool aml_mode_to_resolution(const char *mode, RESOLUTION_INFO *res);
