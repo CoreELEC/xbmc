b96ffe5536722a93a03a3377cfc8e418e81e3f43
Portisch <hugo.portisch@yahoo.de>
2025-06-16 07:48:56 +0200

    AML Windowing: move display items out to AMLDisplay

--- xbmc/guilib/guiinfo/PlayerGUIInfo.cpp
+++ xbmc/guilib/guiinfo/PlayerGUIInfo.cpp
@@ -28,7 +28,6 @@
 #include "guilib/guiinfo/GUIInfoLabels.h"
 #include "resources/LocalizeStrings.h"
 #include "resources/ResourcesComponent.h"
-#include "utils/AMLUtils.h"
 #include "utils/StringUtils.h"
 #include "utils/URIUtils.h"
 #include "utils/Variant.h"
@@ -84,9 +83,10 @@ std::string CPlayerGUIInfo::GetAMLConfigInfo(std::string item) const
 
           if (sub_items.size() > 1)
           {
+            double fps = CServiceBroker::GetWinSystem()->GetGfxContext().GetFPS();
             item_value = StringUtils::Left(sub_items.at(1), sub_items.at(1).length() - 4) + " ";
 
-            if (aml_get_drmProperty("FRAC_RATE_POLICY", DRM_MODE_OBJECT_CONNECTOR))
+            if (fps != floor(fps))
             {
               float refreshrate = static_cast<float>(atof(StringUtils::Mid(sub_items.at(1), sub_items.at(1).length() - 4, 2).c_str())) / 1.001f;
               float refreshrate_rounded = std::round(refreshrate * 1000.0f) / 1000.0f;
