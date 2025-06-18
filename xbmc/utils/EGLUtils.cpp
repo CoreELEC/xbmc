67276f408b81864af2fd961362eb28b8f0d2fe32
Portisch <hugo.portisch@yahoo.de>
2025-06-18 14:13:54 +0200

    EGLUtils: match up config with AML system

--- xbmc/utils/EGLUtils.cpp
+++ xbmc/utils/EGLUtils.cpp
@@ -287,7 +287,7 @@ bool CEGLContextUtils::ChooseConfig(EGLint renderableType, EGLint visualId, bool
                {EGL_GREEN_SIZE, 8},
                {EGL_BLUE_SIZE, 8},
                {EGL_ALPHA_SIZE, 8},
-               {EGL_DEPTH_SIZE, 16},
+               {EGL_DEPTH_SIZE, 24},
                {EGL_STENCIL_SIZE, 0},
                {EGL_SAMPLE_BUFFERS, 0},
                {EGL_SAMPLES, 0},
