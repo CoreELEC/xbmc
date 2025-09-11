64b47b4080ad5e82232cc22702c8785f13a0ebc7
Portisch <hugo.portisch@yahoo.de>
2025-09-11 14:05:29 +0200

    ScreenshotAML: retry video frame capture until valid frame

--- xbmc/utils/ScreenshotAML.cpp
+++ xbmc/utils/ScreenshotAML.cpp
@@ -7,6 +7,7 @@
  */
 
 #include "utils/ScreenshotAML.h"
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -16,14 +17,21 @@
 #include <sys/ioctl.h>
 
 // taken from linux/amlogic/amports/amvideocap.h - needs to be synced - no changes expected though
-#define AMVIDEOCAP_IOC_MAGIC  'V'
-#define AMVIDEOCAP_IOW_SET_WANTFRAME_WIDTH      _IOW(AMVIDEOCAP_IOC_MAGIC, 0x02, int)
-#define AMVIDEOCAP_IOW_SET_WANTFRAME_HEIGHT     _IOW(AMVIDEOCAP_IOC_MAGIC, 0x03, int)
-#define AMVIDEOCAP_IOW_SET_CANCEL_CAPTURE       _IOW(AMVIDEOCAP_IOC_MAGIC, 0x33, int)
+#define AMVIDEOCAP_IOC_MAGIC    'V'
+#define CAP_FLAG_AT_CURRENT      0
+#define CAP_FLAG_AT_TIME_WINDOW  1
+#define CAP_FLAG_AT_END          2
+
+#define AMVIDEOCAP_IOW_SET_WANTFRAME_WIDTH       _IOW(AMVIDEOCAP_IOC_MAGIC, 0x02, int)
+#define AMVIDEOCAP_IOW_SET_WANTFRAME_HEIGHT      _IOW(AMVIDEOCAP_IOC_MAGIC, 0x03, int)
+#define AMVIDEOCAP_IOW_SET_WANTFRAME_WAIT_MAX_MS _IOW(AMVIDEOCAP_IOC_MAGIC, 0x05, unsigned long long)
+#define AMVIDEOCAP_IOW_SET_WANTFRAME_AT_FLAGS    _IOW(AMVIDEOCAP_IOC_MAGIC, 0x06, int)
+#define AMVIDEOCAP_IOW_SET_CANCEL_CAPTURE        _IOW(AMVIDEOCAP_IOC_MAGIC, 0x33, int)
 
 // capture format already defaults to GE2D_FORMAT_S24_RGB - no need to pull in all the ge2d headers :)
 
 #define CAPTURE_DEVICEPATH "/dev/amvideocap0"
+#define AMVIDEOCAP_WAIT_MAX_MS 40
 
 //the buffer format is BGRA (4 byte)
 void CScreenshotAML::CaptureVideoFrame(unsigned char *buffer, int iWidth, int iHeight, bool bBlendToBuffer)
@@ -33,53 +41,67 @@ void CScreenshotAML::CaptureVideoFrame(unsigned char *buffer, int iWidth, int iH
   {
     int stride = ((iWidth + 31) & ~31) * 3;
     int buffSize = stride * iHeight;
-    int readSize = 0;
+    int readSize = -1;
     // videobuffer should be rgb according to docu - but it is bgr ...
     unsigned char *videoBuffer = new unsigned char[buffSize];
 
-    if (videoBuffer != NULL)
+    int retry = 100;
+    while (retry > 0 && readSize < 0)
     {
-      // configure destination
-      ioctl(captureFd, AMVIDEOCAP_IOW_SET_WANTFRAME_WIDTH, stride / 3);
-      ioctl(captureFd, AMVIDEOCAP_IOW_SET_WANTFRAME_HEIGHT, iHeight);
-      readSize = pread(captureFd, videoBuffer, buffSize, 0);
-    }
-
-    close(captureFd);
-
-    if (readSize == buffSize)
-    {
-      if (!bBlendToBuffer)
+      if (videoBuffer != NULL)
       {
-        memset(buffer, 0xff, buffSize);
+        // configure destination
+        ioctl(captureFd, AMVIDEOCAP_IOW_SET_WANTFRAME_WIDTH, iWidth);
+        ioctl(captureFd, AMVIDEOCAP_IOW_SET_WANTFRAME_HEIGHT, iHeight);
+        ioctl(captureFd, AMVIDEOCAP_IOW_SET_WANTFRAME_WAIT_MAX_MS, AMVIDEOCAP_WAIT_MAX_MS);
+        ioctl(captureFd, AMVIDEOCAP_IOW_SET_WANTFRAME_AT_FLAGS, CAP_FLAG_AT_END);
+
+        readSize = pread(captureFd, videoBuffer, buffSize, 0);
+
+        if (errno == ENODATA)
+          break;
       }
 
-      for (int y = 0; y < iHeight; ++y)
+      if (readSize == buffSize)
       {
-        unsigned char *videoPtr = videoBuffer + y * stride;
-
-        for (int x = 0; x < iWidth; ++x, buffer += 4, videoPtr += 3)
+        if (!bBlendToBuffer)
         {
-          float alpha = buffer[3] / (float)255;
+          memset(buffer, 0xff, buffSize);
+        }
 
-          if (bBlendToBuffer)
+        for (int y = 0; y < iHeight; ++y)
+        {
+          unsigned char *videoPtr = videoBuffer + y * stride;
+
+          for (int x = 0; x < iWidth; ++x, buffer += 4, videoPtr += 3)
           {
-            //B
-            buffer[0] = alpha * (float)buffer[0] + (1 - alpha) * (float)videoPtr[0];
-            //G
-            buffer[1] = alpha * (float)buffer[1] + (1 - alpha) * (float)videoPtr[1];
-            //R
-            buffer[2] = alpha * (float)buffer[2] + (1 - alpha) * (float)videoPtr[2];
-            //A
-            buffer[3] = 0xff;// we are solid now
-          }
-          else
-          {
-            memcpy(buffer, videoPtr, 3);
+            float alpha = buffer[3] / (float)255;
+
+            if (bBlendToBuffer)
+            {
+              //B
+              buffer[0] = alpha * (float)buffer[0] + (1 - alpha) * (float)videoPtr[0];
+              //G
+              buffer[1] = alpha * (float)buffer[1] + (1 - alpha) * (float)videoPtr[1];
+              //R
+              buffer[2] = alpha * (float)buffer[2] + (1 - alpha) * (float)videoPtr[2];
+              //A
+              buffer[3] = 0xff;// we are solid now
+            }
+            else
+            {
+              memcpy(buffer, videoPtr, 3);
+            }
           }
         }
       }
+      else
+      {
+        usleep(10000);
+        retry--;
+      }
     }
+    close(captureFd);
     delete [] videoBuffer;
   }
 }
