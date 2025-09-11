23536cb7f3e763c89dcb8a398aff35425761d6ca
Portisch <hugo.portisch@yahoo.de>
2025-09-11 14:10:29 +0200

    ScreenshotSurfaceAML: split gui and video capture

--- xbmc/platform/linux/ScreenshotSurfaceAML.cpp
+++ xbmc/platform/linux/ScreenshotSurfaceAML.cpp
@@ -41,36 +41,43 @@ bool CScreenshotSurfaceAML::Capture()
   GLint viewport[4];
   glGetIntegerv(GL_VIEWPORT, viewport);
 
-  m_width  = viewport[2] - viewport[0];
-  m_height = viewport[3] - viewport[1];
+  m_width  = viewport[2];
+  m_height = viewport[3];
   m_stride = m_width * 4;
   unsigned char* surface = new unsigned char[m_stride * m_height];
 
   //read pixels from the backbuffer
 #if HAS_GLES >= 2
-  glReadPixels(viewport[0], viewport[1], viewport[2], viewport[3], GL_RGBA, GL_UNSIGNED_BYTE, (GLvoid*)surface);
+  glReadPixels(0, 0, m_width, m_height, GL_RGBA, GL_UNSIGNED_BYTE, (GLvoid*)surface);
 #else
-  glReadPixels(viewport[0], viewport[1], viewport[2], viewport[3], GL_BGRA, GL_UNSIGNED_BYTE, (GLvoid*)surface);
+  glReadPixels(0, 0, m_width, m_height, GL_BGRA, GL_UNSIGNED_BYTE, (GLvoid*)surface);
 #endif
 
-  //make a new buffer and copy the read image to it with the Y axis inverted
+  if (glGetError() != GL_NO_ERROR)
+  {
+    CLog::Log(LOGERROR, "CScreenshotSurfaceAML: glReadPixels failed");
+    delete[] surface;
+    return false;
+  }
+
   m_buffer = new unsigned char[m_stride * m_height];
   for (int y = 0; y < m_height; y++)
   {
 #ifdef HAS_GLES
     // we need to save in BGRA order so XOR Swap RGBA -> BGRA
     unsigned char* swap_pixels = surface + (m_height - y - 1) * m_stride;
-    for (int x = 0; x < m_width; x++, swap_pixels+=4)
-    {
+    for (int x = 0; x < m_width; x++, swap_pixels += 4)
       std::swap(swap_pixels[0], swap_pixels[2]);
-    }
 #endif
-    memcpy(m_buffer + y * m_stride, surface + (m_height - y - 1) *m_stride, m_stride);
+    memcpy(m_buffer + y * m_stride, surface + (m_height - y - 1) * m_stride, m_stride);
   }
 
-  delete [] surface;
+  delete[] surface;
+  return true;
+}
 
+void CScreenshotSurfaceAML::CaptureVideo(bool blendToBuffer)
+{
   // Captures the current visible videobuffer and blend it into m_buffer (captured overlay)
   CScreenshotAML::CaptureVideoFrame(m_buffer, m_width, m_height);
-  return true;
 }
