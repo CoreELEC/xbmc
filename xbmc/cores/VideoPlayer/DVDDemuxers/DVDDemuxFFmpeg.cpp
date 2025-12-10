cff42b8657e222350d75c4a07ab72e122faebdb4
Portisch <hugo.portisch@yahoo.de>
2025-12-10 20:28:15 +0100

    DVDDemuxFFmpeg: fix AVS2 decode

--- xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
+++ xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
@@ -2635,7 +2635,8 @@ TRANSPORT_STREAM_STATE CDVDDemuxFFmpeg::TransportStreamVideoState()
       if (st->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
       {
         if (idx == m_pkt.pkt.stream_index && m_pkt.pkt.dts != AV_NOPTS_VALUE &&
-            (st->codecpar->extradata || (st->codecpar->codec_id == AV_CODEC_ID_VVC)))
+            (st->codecpar->extradata || (st->codecpar->codec_id == AV_CODEC_ID_VVC) ||
+            (st->codecpar->codec_id == AV_CODEC_ID_AVS2)))
         {
           if (!m_startTime)
           {
@@ -2657,7 +2658,8 @@ TRANSPORT_STREAM_STATE CDVDDemuxFFmpeg::TransportStreamVideoState()
       if (st->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
       {
         if (static_cast<int>(i) == m_pkt.pkt.stream_index && m_pkt.pkt.dts != AV_NOPTS_VALUE &&
-            (st->codecpar->extradata || (st->codecpar->codec_id == AV_CODEC_ID_VVC)))
+            (st->codecpar->extradata || (st->codecpar->codec_id == AV_CODEC_ID_VVC) ||
+            (st->codecpar->codec_id == AV_CODEC_ID_AVS2)))
         {
           if (!m_startTime)
           {
