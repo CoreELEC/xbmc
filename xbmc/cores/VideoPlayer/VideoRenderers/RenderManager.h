27b6170b66e8d56b1e0b1aafb92a945a3870c537
Portisch <hugo.portisch@yahoo.de>
2026-05-18 15:16:09 +0200

    Render: take care of frame wait before display mode change Use decoder timeout also for render timeout.

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.h
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.h
@@ -213,6 +213,7 @@ protected:
   unsigned int m_orientation = 0;
   int m_NumberBuffers = 0;
   int m_lateframes = -1;
+  int m_amdv_wait_delay = -1;
   double m_presentpts = 0.0;
   EPRESENTSTEP m_presentstep = PRESENT_IDLE;
   XbmcThreads::EndTime<> m_presentTimer;
@@ -224,6 +225,7 @@ protected:
   CEvent m_initEvent;
   CDVDClock &m_dvdClock;
   IRenderMsg *m_playerPort;
+  int m_render_timeout;
 
   struct CClockSync
   {
