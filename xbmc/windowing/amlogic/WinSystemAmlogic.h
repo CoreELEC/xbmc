b49413fa2644e0858689a0527fd2ae028994a6b6
Portisch <hugo.portisch@yahoo.de>
2026-06-20 15:47:56 +0200

    AMLDisplay: drop support of FBDEV
    Focus on GBM windowing only.
    FBDEV becomes obsolete so prefer GBM/Wayland libMali blob.

--- xbmc/windowing/amlogic/WinSystemAmlogic.h
+++ xbmc/windowing/amlogic/WinSystemAmlogic.h
@@ -15,27 +15,9 @@
 #include "windowing/WinSystem.h"
 #include "threads/SystemClock.h"
 #include "system_egl.h"
-#include <EGL/fbdev_window.h>
 #include <gbm.h>
 
 class IDispResource;
-
-class DllMaliInterface
-{
-public:
-  virtual ~DllMaliInterface() = default;
-  virtual struct gbm_device *gbm_create_device(int fd) = 0;
-};
-
-class DllMali : public DllDynamic, public DllMaliInterface
-{
-public:
-  DECLARE_DLL_WRAPPER(DllMali, "libMali.so")
-  DEFINE_METHOD1(struct gbm_device *, gbm_create_device, (int p1))
-  BEGIN_METHOD_RESOLVE()
-    RESOLVE_METHOD(gbm_create_device)
-  END_METHOD_RESOLVE()
-};
 
 class CWinSystemAmlogic : public CWinSystemBase
 {
@@ -71,7 +53,6 @@ public:
 protected:
   std::string m_framebuffer_name;
   EGLDisplay m_nativeDisplay;
-  fbdev_window *m_nativeWindow;
 
   RenderStereoMode m_stereo_mode;
 
