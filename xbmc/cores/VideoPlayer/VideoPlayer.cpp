ca1e772a06c88fbaf793bd1083f6008955fdfb8e
Portisch <hugo.portisch@yahoo.de>
2026-07-02 13:12:17 +0200

    VideoPlayer: include subtitle stream of bluray in continuity checks
    libbluray may drop PTS/DTS on subtitle packets, causing wrong subtitle start times.

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -2127,6 +2127,8 @@ void CVideoPlayer::ProcessSubData(CDemuxStream* pStream, DemuxPacket* pPacket)
 {
   CheckStreamChanges(m_CurrentSubtitle, pStream);
 
+  if (m_pInputStream && m_pInputStream->IsStreamType(DVDSTREAM_TYPE_BLURAY))
+    CheckContinuity(m_CurrentSubtitle, pPacket);
   UpdateTimestamps(m_CurrentSubtitle, pPacket);
 
   bool drop = false;
@@ -2756,6 +2758,7 @@ bool CVideoPlayer::CheckContinuity(CCurrentStream& current, DemuxPacket* pPacket
         current.type == StreamType::AUDIO ? m_CurrentVideo.lastdts : m_CurrentAudio.lastdts;
 
     if (m_CurrentAudio.id == -1 || m_CurrentVideo.id == -1 ||
+       current.type == StreamType::SUBTITLE ||
        current.lastdts == DVD_NOPTS_VALUE ||
        fabs(this_dts - that_dts) < DVD_MSEC_TO_TIME(1000))
     {
