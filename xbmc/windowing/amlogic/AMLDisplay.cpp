9ee7275de80c75969c331aa313745f53135b53df
Emmanuel André <306831579+manuandre@users.noreply.github.com>
2026-08-22 00:25:39 +0200

    Amlogic: refresh connector state after hotplug recovery

--- xbmc/windowing/amlogic/AMLDisplay.cpp
+++ xbmc/windowing/amlogic/AMLDisplay.cpp
@@ -562,6 +562,16 @@ bool CAMLDRMUtils::aml_set_drmDevice_mode(const RESOLUTION_INFO &res, std::strin
   int fractional_rate = (res.fRefreshRate == floor(res.fRefreshRate)) ? 0 : 1;
   ret = aml_set_drmDevice_active(mode, fractional_rate, stereo_mode, _force_mode_switch, true);
 
+  if (ret && _hotplug_mode_switch)
+  {
+    drmModeConnectorPtr connector = drmModeGetConnector(m_fd, m_connector->connector_id);
+    if (connector)
+    {
+      m_connection = connector->encoder_id ? DRM_MODE_CONNECTED : DRM_MODE_DISCONNECTED;
+      drmModeFreeConnector(connector);
+    }
+  }
+
   if (ret)
   {
     if (_force_mode_switch)
