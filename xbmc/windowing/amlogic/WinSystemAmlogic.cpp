50e0261211957d9c3a43c5303d233f2fdabb0564
Emmanuel André <306831579+manuandre@users.noreply.github.com>
2026-08-24 22:48:01 +0200

    Amlogic: defer early DRM hotplug until render loop

--- xbmc/windowing/amlogic/WinSystemAmlogic.cpp
+++ xbmc/windowing/amlogic/WinSystemAmlogic.cpp
@@ -350,8 +350,8 @@ bool CWinSystemAmlogic::InitWindowSystem()
     if (mode_count > 1)
     {
       CLog::Log(LOGWARNING,
-        "CWinSystemAmlogic::InitWindowSystem HDMI modes are present but DRM connector is not ready, trigger hotplug");
-      HotplugEvent();
+        "CWinSystemAmlogic::InitWindowSystem HDMI modes are present but DRM connector is not ready, defer hotplug");
+      m_hotplugPending.store(true);
     }
     else if (mode_count == 1)
     {
