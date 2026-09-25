6f82998088fa16c9566ceb15773121817e5e30d7
matthane <matthan.evans@icloud.com>
2026-09-25 12:38:23 -0500

    VideoPlayer: fix SDR PGS white under PQ output
    Under Dolby Vision or HDR10 output, SDR PGS of a source converted to
    Dolby Vision, or of a track titled SDR, is drawn at GUI peak luminance,
    so at a low setting it is gray next to the video's white.

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -4467,6 +4467,26 @@ bool CVideoPlayer::OpenStream(CCurrentStream& current, int64_t demuxerId, int iS
           hint.colorPrimaries = m_CurrentVideo.hint.colorPrimaries;
           hint.colorTransferCharacteristic = m_CurrentVideo.hint.colorTransferCharacteristic;
         }
+        if ((m_CurrentVideo.hint.hdrType == StreamHdrType::HDR_TYPE_DOLBYVISION ||
+             m_CurrentVideo.hint.colorTransferCharacteristic == AVCOL_TRC_SMPTE2084) &&
+            hint.colorTransferCharacteristic != AVCOL_TRC_SMPTE2084 &&
+            hint.colorTransferCharacteristic != AVCOL_TRC_ARIB_STD_B67)
+        {
+          // the DV driver's SDR graphics level under DV output, else BT.2408 graphics white
+          constexpr int DV_SDR_GRAPHICS_WHITE = 300;
+          constexpr int REFERENCE_WHITE = 203;
+          // hdr2sdr output is SDR even while the GUI composite runs as PQ,
+          // unless Dolby Vision output takes precedence, as in RendererAML
+          const bool hdr2sdr = CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(
+              CSettings::SETTING_COREELEC_AMLOGIC_HDR2SDR);
+          const bool dvOutput =
+              m_CurrentVideo.hint.hdrType == StreamHdrType::HDR_TYPE_DOLBYVISION &&
+              aml_dolby_vision_enabled();
+          if (videoHdrType == StreamHdrType::HDR_TYPE_NONE)
+            hint.sdrWhiteNits = DV_SDR_GRAPHICS_WHITE;
+          else if (!hdr2sdr || dvOutput)
+            hint.sdrWhiteNits = REFERENCE_WHITE;
+        }
       }
       res = OpenSubtitleStream(hint);
       break;
